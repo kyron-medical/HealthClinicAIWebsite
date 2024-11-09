37af7eee0bb42c65981c27c60c958d693c9b02df
alejack312 <alejack312@gmail.com>
2024-11-09 01:35:51 -0500

    feat: beginning implementations of auth; feat: demo page creation

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      {children}
-    </ThemeProvider>
+    <ClerkProvider>
+      <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+        {children}
+      </ThemeProvider>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Demo/index.tsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React, { useState } from "react";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+const Contact = () => {
+  const [name, setName] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+  const handleSubmit: React.MouseEventHandler<HTMLButtonElement> = async (
+    event,
+  ) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("/api/sendEmail", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, subject, message }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        window.location.href = data.url; // Redirect to Gmail
+        console.log("Email sent successfully");
+      } else {
+        console.error("Failed to send email", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error: Failed to fetch", error);
+    }
+  };
+
+  return (
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+      <div className="container">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+              <div
+                className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+                data-wow-delay=".15s
+              "
+              >
+                <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                  Request a Demo
+                </h2>
+                <p className="mb-12 text-base font-medium text-body-color">
+                  Our support team will get back to you ASAP via email.
+                </p>
+                <form>
+                  <div className="-mx-4 flex flex-wrap">
+                    <div className="w-full px-4 md:w-1/2">
+                      <div className="mb-8">
+                        <label
+                          htmlFor="name"
+                          className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        >
+                          Your Name
+                        </label>
+                        <input
+                          type="text"
+                          placeholder="Enter your name"
+                          className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full px-4 md:w-1/2">
+                      <div className="mb-8">
+                        <label
+                          htmlFor="subject"
+                          className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        >
+                          Subject
+                        </label>
+                        <input
+                          type="subject"
+                          placeholder="Enter your Subject"
+                          className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                          value={subject}
+                          onChange={(e) => setSubject(e.target.value)}
+                        />
+                      </div>
+                    </div>
+                    <div className="w-full px-4">
+                      <div className="mb-8">
+                        <label
+                          htmlFor="message"
+                          className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                        >
+                          Your Message
+                        </label>
+                        <textarea
+                          name="message"
+                          rows={5}
+                          placeholder="Enter your Message"
+                          className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                          value={message}
+                          onChange={(e) => setMessage(e.target.value)}
+                        ></textarea>
+                      </div>
+                    </div>
+                    <div className="w-full px-4">
+                      <button
+                        type="button"
+                        className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark"
+                        onClick={handleSubmit}
+                      >
+                        Submit Ticket
+                      </button>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </SignedIn>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
