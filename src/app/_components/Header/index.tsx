adddf1b1f2f49de99d9dea9710fe10b376f7fdec
Alejandro Jackson <alejack312@gmail.com>
2025-01-09 14:41:53 -0500

    fix: fix header bar size; fix: sign in button size in header bar; feat: changed pages to server-side pages; chore: imported infinite carousel component

--- src/app/_components/Header/index.tsx
+++ src/app/_components/Header/index.tsx
@@ -45,7 +45,7 @@ const Header = () => {
         className={`header left-0 top-0 z-40 flex w-full items-center ${
           sticky
             ? "fixed z-[9999] bg-white !bg-opacity-80 shadow-sticky backdrop-blur-sm transition dark:bg-gray-dark dark:shadow-sticky-dark"
-            : "absolute bg-white bg-opacity-90"
+            : "absolute bg-white bg-opacity-90 h-20"
         }`}
       >
         <div className="container">
@@ -162,7 +162,6 @@ const Header = () => {
                   </ul>
                 </nav>
               </div>
-            </div>{" "}
             <SignedOut>
               <div className="flex justify-end">
                 <SignInButton />
@@ -173,6 +172,7 @@ const Header = () => {
                 <UserButton />
               </div>
             </SignedIn>
+            </div>{" "}
             {/*<div className="flex items-center justify-end pr-16 lg:pr-0">
                 <Link
                   href="/signin"
--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,16 +1,18 @@
-import React from "react";
 import styles from "./NewsPage.module.css";
 import SectionTitle from "@/components/Common/SectionTitle";
 import SingleBlog from "@/components/Blog/SingleBlog";
 import newsData from "@/components/News/newsData"; // Replace this with the relevant data for news articles
 
 import { Metadata } from "next";
+import { InfiniteCarousel } from "../_components/InfiniteCarousel";
 
-export const metadata: Metadata = {
-  title: "News Page | Kyron",
-  description: "News Page for Kyron",
-  // other metadata
-};
+// export const metadata: Metadata = {
+//   title: "News Page | Kyron",
+//   description: "News Page for Kyron",
+//   // other metadata
+// };
+
+
 
 const NewsPage = () => {
   return (
@@ -24,6 +26,10 @@ const NewsPage = () => {
           paragraph="Stay up to date with the latest news and updates for Kyron!"
           center
         />
+
+        <div>
+          <InfiniteCarousel children={[]}/>
+        </div>
 
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
           {newsData.map((newsItem) => (
