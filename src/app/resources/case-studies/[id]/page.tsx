d436095d06448b0b8d66c138ca6b04f6ea5d0f7f
Alejandro <alejack312@gmail.com>
2025-04-21 18:49:56 -0400

    fix: remove unnecessary pages

--- src/app/resources/case-studies/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import FullPageImageView from "../../_components/full-image-page";
-
-export default async function PositionPage({
-  params: { id: resourceId },
-}: {
-  params: { id: string };
-}) {
-  return <FullPageImageView resourceId={resourceId} resourceType="Case Study" />;
-}
